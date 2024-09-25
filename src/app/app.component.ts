9cedfcf562aa62b6cbf5daa0cea5fa0be851a232
AlexiaGu <150676364+AlexiaGu@users.noreply.github.com>
2024-09-25 15:38:28 +0200

    Quête Angular 02 - Les composants

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,13 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { ArticleComponent } from './article/article.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, ArticleComponent],
   templateUrl: './app.component.html',
+  //  template: '<h1>"hello you"</h1>',
   styleUrl: './app.component.scss',
 })
 export class AppComponent {

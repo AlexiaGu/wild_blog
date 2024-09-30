85d45b280b3cc9f9308dde06932411e02233eea6
AlexiaGu <150676364+AlexiaGu@users.noreply.github.com>
2024-09-30 17:06:12 +0200

    Quête Angular 06 - Les templates Driven Forms

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,10 +2,12 @@ import { Routes } from '@angular/router';
 import { HomePageComponent } from './home-page/home-page.component';
 import { NotFoundComponent } from './not-found/not-found.component';
 import { ArticlePageComponent } from './article-page/article-page.component';
+import { ContactFormComponent } from './contact-form/contact-form.component';
 
 export const routes: Routes = [
   { path: '', component: HomePageComponent },
   { path: 'article/:id', component: ArticlePageComponent },
+  { path: 'contactform', component: ContactFormComponent },
   //chemin page 404 Not found
   { path: '**', component: NotFoundComponent },
 ];

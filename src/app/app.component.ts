cb17c990bfcdd25e4553a694450a59ca4adb8a83
AlexiaGu <150676364+AlexiaGu@users.noreply.github.com>
2024-10-02 11:36:17 +0200

    Quête Angular 07 - Les Reactives Forms

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,12 @@
 import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { RouterOutlet, RouterLink, Router } from '@angular/router';
 // import { ArticleComponent } from './article/article.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, CommonModule],
+  imports: [RouterOutlet, CommonModule, RouterLink],
   templateUrl: './app.component.html',
   //  template: '<h1>"hello you"</h1>',
   styleUrl: './app.component.scss',

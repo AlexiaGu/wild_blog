800438973c348c5ed70d166985b76e6bfff4f713
AlexiaGu <150676364+AlexiaGu@users.noreply.github.com>
2024-10-03 17:34:33 +0200

    Quête Angular 09 - Communication entre les composants (enfant vers parent) : @Output

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,9 +1,9 @@
 import { Routes } from '@angular/router';
-import { HomePageComponent } from './home-page/home-page.component';
-import { NotFoundComponent } from './not-found/not-found.component';
-import { ArticlePageComponent } from './article-page/article-page.component';
-import { ContactFormComponent } from './contact-form/contact-form.component';
-import { SignupFormComponent } from './signup-form/signup-form.component';
+import { NotFoundComponent } from './pages/not-found/not-found.component';
+import { ArticlePageComponent } from './pages/article-page/article-page.component';
+import { SignupFormComponent } from './pages/signup-form/signup-form.component';
+import { HomePageComponent } from './pages/home-page/home-page.component';
+import { ContactFormComponent } from './pages/contact-form/contact-form.component';
 
 export const routes: Routes = [
   { path: '', component: HomePageComponent },
